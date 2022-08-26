c245627fc47add19a710e62127543284f45a801b
Sreeraj-palat <work.sreeraj@gmail.com>
2022-08-26 19:26:42 +0530

    review model added

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,6 @@
 from xml.parsers.expat import model
 from django.contrib import admin
-from .models import Product, Variation
-
+from .models import Product, Variation,ReviewRating
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
@@ -15,3 +14,4 @@ class VariationAdmin(admin.ModelAdmin):
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation,VariationAdmin)
+admin.site.register(ReviewRating)
